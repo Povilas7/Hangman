aa36a8600f6d7aea9dbf125609167f13545da21f
unknown <povilas.pofka54@gmail.com>
2021-03-14 23:02:57 +0200

    finish

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let hangman = {
     guesses: 6,
     dictionary: ["medis", "televizorius", "medalis", "sausainis", "traktoristas"],
@@ -34,4 +35,70 @@ let hangman = {
         hangman.reset();
     },
 
-}
+    toggle : function (disable) {
+        let all = hangman.hChar.getElementsByTagName("input");
+        for (let i of all) { i.disabled = disable; }
+    },
+
+    reset : function () {
+        hangman.rights = 0;
+        hangman.wrongs = 0;
+        hangman.hLives.innerHTML = hangman.guesses;
+        hangman.hImg.style.opacity = 0;
+
+        hangman.word = hangman.dictionary[Math.floor(Math.random() * Math.floor(hangman.dictionary.length))];
+        hangman.word = hangman.word.toUpperCase();
+        hangman.wordlen = hangman.word.length;
+
+        hangman.hWord.innerHTML = "";
+        for (let i=0; i<hangman.word.length; i++) {
+            let charnow = document.createElement("span");
+            charnow.innerHTML = "_";
+            charnow.id = "hangword-" + i;
+            hangman.hWord.appendChild(charnow);
+        }
+
+        hangman.toggle(false);
+    },
+
+    check : function () {
+        let index = 0, hits = [];
+        while (index >= 0) {
+            index = hangman.word.indexOf(this.value, index);
+            if (index === -1) { break; }
+            else {
+                hits.push(index);
+                index++;
+            }
+        }
+
+        if (hits.length > 0) {
+            for (let hit of hits) {
+                document.getElementById("hangword-" + hit).innerHTML = this.value;
+            }
+
+            hangman.rights += hits.length;
+            if (hangman.rights === hangman.wordlen) {
+                hangman.toggle(true);
+                alert("YOU WIN!");
+            }
+        }
+
+        else {
+            hangman.wrongs++;
+            let livesleft = hangman.guesses - hangman.wrongs;
+            hangman.hLives.innerHTML = livesleft;
+            hangman.hImg.style.opacity = (1 - (livesleft/hangman.guesses)).toFixed(2);
+
+            if (hangman.wrongs === hangman.guesses) {
+                hangman.toggle(true);
+                alert("YOU LOSE!");
+            }
+        }
+
+    }
+};
+
+window.addEventListener("DOMContentLoaded", hangman.init);
+
+
